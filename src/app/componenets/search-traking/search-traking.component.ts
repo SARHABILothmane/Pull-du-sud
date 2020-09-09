2a1d8460eced50211a77cddac481526f274ca7c7
SARHABILothmane <othmanos2016.aya@gmail.com>
2020-09-09 08:50:28 +0100

    first commit

---
+++ src/app/componenets/search-traking/search-traking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-traking',
+  templateUrl: './search-traking.component.html',
+  styleUrls: ['./search-traking.component.css']
+})
+export class SearchTrakingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
